7c5f5a566aec562961e38ae30a3bb51171d85b84
Obai Alnajjar <obai@outlook.com>
2019-03-18 23:05:47 +0300

    Added sign in functionality

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,8 +1,20 @@
 const db = require('../models');
 const jwt = require('jsonwebtoken');
 
-exports.signIn = async function () {
-	
+exports.signIn = async function (req, res, next) {
+	try {
+		let user = await db.User.findOne({
+			email: req.body.email
+		});
+		let {id, username, profileImage} = user;
+		if (await user.comparePassword(req.body.password)) {
+			let token = jwt.sign({id, username, profileImage}, process.env.SECRET_KEY);
+			return res.status(200).json({id, username, profileImage, token});
+		}
+		return next({status: 400, message: 'Invalid Email/Password!'});
+	} catch (e) {
+		return next({status: 400, message: 'Invalid Email/Password!'});
+	}
 };
 
 exports.signUp = async function (req, res, next) {
