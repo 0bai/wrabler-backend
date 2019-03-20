26a756d59114dd54c5abf83a5381b6d4d692cb00
Obai Alnajjar <obai@outlook.com>
2019-03-20 22:37:02 +0300

    Created ensureCorrectUser method

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,18 +4,35 @@ const jwt = require('jsonwebtoken');
 
 exports.loginRequired = function (req, res, next) {
 	try {
-		const token = req.headers.authorization.split(" ")[ 1 ];
+		const token = getToken(req);
 		jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
 			if (decoded) return next();
-			return next({
-				status: 401,
-				message: 'Please login first!'
-			});
+			return next(generateErrorResponse(401, 'Please login first!'));
 		});
 	} catch (e) {
-		return next({
-			status: 401,
-			message: 'Please login first!'
-		});
+		return next(generateErrorResponse(401, 'Please login first!'));
 	}
 };
+
+exports.ensureCorrectUser = function (req, res, next) {
+	try {
+		const token = getToken(req);
+		jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+			if (decoded && decoded.id === req.params.id) return next();
+			return next(generateErrorResponse(401, "You don't have permission to do that!"));
+		});
+	} catch (e) {
+		return next(generateErrorResponse(401, "You don't have permission to do that!"));
+	}
+};
+
+function generateErrorResponse(code, message) {
+	return {
+		status: code,
+		message: message
+	};
+}
+
+function getToken(req) {
+	return req.headers.authorization.split(" ")[ 1 ];
+}
