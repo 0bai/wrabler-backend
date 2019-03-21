2c1c4d5d30b2b55dae09d056fdcd20fb94c1396a
Obai Alnajjar <obai@outlook.com>
2019-03-21 12:31:15 +0300

    Added create & delete message functionality

--- helpers/messages.js
+++ helpers/messages.js
@@ -20,6 +20,19 @@ exports.createMessage = async function (req, res, next) {
 	}
 };
 exports.getMessage = async function (req, res, next) {
+	try {
+		let message = await db.Message.findById(req.params.message_id);
+		return res.status(200).json(message);
+	} catch (e) {
+		next(e);
+	}
 };
 exports.deleteMessage = async function (req, res, next) {
+	try {
+		let message = await db.Message.findById(req.params.message_id);
+		await message.remove();
+		return res.status(200).json(message);
+	} catch (e) {
+		next(e);
+	}
 };
